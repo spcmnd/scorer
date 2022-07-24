4e4ef8b0bf21a8031b9483ef54ff3b02e8e704d7
spcmnd <spcmndd@protonmail.com>
2022-07-24 11:45:49 +0200

    Remove unused derive

--- src/models/player.rs
+++ src/models/player.rs
@@ -4,19 +4,19 @@ use validator::Validate;
 
 use crate::schema::players;
 
-#[derive(Debug, Clone, Queryable, Serialize, Deserialize, Insertable)]
+#[derive(Debug, Clone, Queryable, Serialize, Insertable)]
 pub struct Player {
     pub id: String,
     pub name: String,
 }
 
-#[derive(Serialize, Deserialize, Validate)]
+#[derive(Deserialize, Validate)]
 pub struct PlayerRequestBody {
     #[validate(required, length(min = 3, max = 32))]
     pub name: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Validate)]
+#[derive(Deserialize, Validate)]
 pub struct PlayerUpdateRequestBody {
     #[validate(required, length(min = 3, max = 32))]
     pub name: Option<String>,
